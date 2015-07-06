b1d64f2c7ce2ed56355048453be912d2f1020923
Kody Law <klaw1794@gmail.com>
2015-07-06 10:15:02 +0800

    Update punishment.js

--- punishment.js
+++ punishment.js
@@ -1,24 +1,6 @@
 var punishment = function(){
-    punish=Math.random();
-    if (punish<(1/9)){
-        return "buying or making everyone coffee.";
-    } else if (punish<(2/9)){
-        return "writing a blog post for this class.";
-    } else if (punish<(3/9)){
-        return "coming to next class in a formal attire.";
-    } else if (punish<(4/9)){
-        return "ending each conversation with 'winter is coming' until the end of lunch.";
-    } else if (punish<(5/9)){
-        return "singing and dancing in front of the class.";
-    } else if (punish<(6/9)){
-        return "bowing to everyone and apologizing seriously.";
-    } else if (punish<(7/9)){
-        return "standing in the corner for one hour.";
-    } else if (punish<(8/9)){
-        return "doing two push ups for every minute late.";
-    } else {
-        return "jogging to the ferry and taking a selfie as proof.";
-    }
+    punishArray=["buying or making everyone coffee.","writing a blog post for this class.","coming to next class in a formal attire.","ending each conversation with 'winter is coming' until the end of lunch.","singing and dancing in front of the class.","bowing to everyone and apologizing seriously.","standing in the corner for one hour.","doing two push ups for every minute late.","jogging to the ferry and taking a selfie as proof."];
+    return punishArray[Math.floor(Math.random() * punishArray.length)];
 };
 
 console.log("The punishment is: "+punishment());
