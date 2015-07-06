2f2616fa16decacb45c1976aa8193703e09a6155
raptattention <klaw1794@gmail.com>
2015-07-06 13:30:34 +0800

    New HTML and JS

--- punishment.js
+++ punishment.js
@@ -1,5 +1,12 @@
 var punishment = function(){
     punishArray=["buying or making everyone coffee.","writing a blog post for this class.","coming to next class in a formal attire.","ending each conversation with 'winter is coming' until the end of lunch.","singing and dancing in front of the class.","bowing to everyone and apologizing seriously.","standing in the corner for one hour.","doing two push ups for every minute late.","jogging to the ferry and taking a selfie as proof."];
     punish = punishArray[Math.floor(Math.random() * punishArray.length)];
-    alert("The punishment is: "+punish);
+    return punish;
 };
+
+var myButton = document.getElementById("punisher");
+
+myButton.addEventListener("click", function(){
+	elem = document.getElementById("ready_punish");
+	elem.innerHTML = "The punishment is " + punishment();
+}) 
