ae569ce38b294a7e7caa687eb48d2756daf3b71c
Kody Law <klaw1794@gmail.com>
2015-07-06 10:40:52 +0800

    Update punishment.js

--- punishment.js
+++ punishment.js
@@ -1,6 +1,5 @@
 var punishment = function(){
     punishArray=["buying or making everyone coffee.","writing a blog post for this class.","coming to next class in a formal attire.","ending each conversation with 'winter is coming' until the end of lunch.","singing and dancing in front of the class.","bowing to everyone and apologizing seriously.","standing in the corner for one hour.","doing two push ups for every minute late.","jogging to the ferry and taking a selfie as proof."];
-    return punishArray[Math.floor(Math.random() * punishArray.length)];
+    punish = punishArray[Math.floor(Math.random() * punishArray.length)];
+    alert("The punishment is: "+punish);
 };
-
-console.log("The punishment is: "+punishment());
