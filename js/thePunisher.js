a199770d3a1591d88734e7aca7815af8c6e6867d
teelu <terrence.lun@gmail.com>
2015-07-07 01:47:48 +0800

    Finished punisher

--- js/thePunisher.js
+++ js/thePunisher.js
@@ -6,7 +6,7 @@ var punishment = function(){
   var punishmentArr = ["Buy/Serve Coffee for Everyone", "Write a blog post", 
   					   "Come to next class in formal attire", "End conversations with \"Lunch is coming\" until lunchtime",
   						"Sing/Dance infront of the class", "Stand in the corner for an hour", 
-  						"Bow to everyone and apologize sincerely", "Push up for every minute late", 
+  						"Bow to everyone and apologize sincerely", "do a push up for every minute late", 
   						"Jog to ferry pier during lunch and take a selfie"];
    
    return punishmentArr[Math.floor((Math.random()*punishmentArr.length))];
@@ -14,5 +14,5 @@ var punishment = function(){
 }
 
 document.getElementById('getPunishment').addEventListener("click", function() {
-	document.getElementById('thePunisher').innerHTML = punishment();
+	document.getElementById('thePunisher').innerHTML = "You must " + punishment();
 });
